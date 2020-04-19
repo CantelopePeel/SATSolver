f4cd1139c4193e5802a772c1e11ad93c1402077d
Liam Heeger <lheeger@u.rochester.edu>
2020-04-19 01:35:13 -0400

    Updates to command line parameters, Added stubs for cdcl and dpll solvers.

---
+++ solvers/cdcl_solver.cc
@@ -0,0 +1,13 @@
+//
+// Created by Liam Heeger on 2020-04-18.
+//
+
+#include "cdcl_solver.h"
+
+using namespace sat_solver;
+
+SATState
+CDCLSolver::
+check(const ClauseList& clause_list, Assignment* assignment) {
+    return UNDEF;
+}
