b0f1f309f6b070f5611957aa78f4702c84f15ad3
Shreif Abdallah <shreifelsaid@gmail.com>
2020-04-21 17:52:17 -0400

    start schoning

---
+++ solvers/schoning_solver.cc
@@ -0,0 +1,10 @@
+#include "schoning_solver.h"
+
+using namespace sat_solver;
+
+SATState
+schoningSolver::
+    check(const sat_solver::ClauseList &clause_list, sat_solver::Assignment *assignment)
+{
+    return UNDEF;
+}
---
+++ solvers/schoning_solver.h
@@ -0,0 +1,17 @@
+#ifndef SATSOLVER_SCHONING_SOLVER_H
+#define SATSOLVER_SCHONING_SOLVER_H
+
+#include "solver.h"
+
+namespace sat_solver {
+
+class schoningSolver : public Solver {
+    public:
+        SATState check(const ClauseList& clause_list, Assignment* assignment) override;
+
+};
+
+}
+
+
+#endif 
