e6c304aa85f399acb4bc60fe4b38a129351b1763
Shreif Abdallah <shreifelsaid@gmail.com>
2020-04-22 13:58:50 -0400

    updated check function

--- solvers/schoning_solver.cc
+++ solvers/schoning_solver.cc
@@ -18,6 +18,20 @@ get_unassigned_variable(const ClauseList &clause_list, const Assignment &assignm
     return false;
 }
 
+
+static bool
+get_unsatisfied_clause(const ClauseList &clause_list, const Assignment &assignment, Variable *variable)
+{
+    for (Variable var : clause_list.variables())
+    {
+        if (!assignment.contains(var))
+        {
+            *variable = var;
+            return true;
+        }
+    }
+    return false;
+}
 static SATState
 determine_sat_state(const ClauseList &clause_list, const Assignment &assignment)
 {
@@ -74,8 +88,21 @@ schoningSolver::
     const long unsigned int number_of_clauses = clause_list.num_clauses();
     //while there is an unassigned variable in the clause list, reassign it randomly between true and false.
     Variable var(0);
-    while (get_unassigned_variable(clause_list, *assignment, &var)) {}
+    while (get_unassigned_variable(clause_list, *assignment, &var)) {
+        if(rand()%2){
+            Literal pos_literal(false, var);
+            assignment->push_literal(pos_literal);
+        }else{
+            Literal neg_literal(true, var);
+            assignment->push_literal(neg_literal);
+        }
+    }
+    for(int i = 0; i < number_of_clauses*3; i++){
+        SATState sat_state = determine_sat_state(clause_list, *assignment);
+        if (sat_state == SATState::SAT){
+            return sat_state;
+        }
+    }
     
-
     return UNDEF;
 }
