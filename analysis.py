c6f4cc19d90ec17605c65b8cb09de449ec10e477
vlade <vlade.maksimovski@gmail.com>
2020-04-26 16:41:39 -0400

    analysis saves results to png

--- analysis.py
+++ analysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def analyze():
-    df = pd.read_csv("./gen_run.csv", index_col=0)    
+    df = pd.read_csv("./gen_run.csv", index_col=0)
     print("DPLL correct:\n", (df["bt_state"] == df["dpll_state"]).value_counts())
     print("CDCL correct:\n", (df["bt_state"] == df["cdcl_state"]).value_counts())
     print("Schoning correct:\n", (df["bt_state"] == df["scho_state"]).value_counts())
@@ -20,6 +20,7 @@ def analyze():
     plt.semilogy(means_time_df)
     plt.legend(means_time_df.columns)
     plt.show()
+    plt.savefig('results.png')
     print(df)
 
 
